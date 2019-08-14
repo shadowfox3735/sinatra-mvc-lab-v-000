4d7746dd205445c0551e60c5f7dc17927f5a39dc
shadowfox3735 <normanpan180@gmail.com>
2019-08-14 00:18:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
   
-  def initialize(input_str)
+  dsef piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
   
