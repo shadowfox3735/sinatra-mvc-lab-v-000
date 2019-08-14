78504996cf80e2e69c4db90712308e923178041e
shadowfox3735 <normanpan180@gmail.com>
2019-08-14 00:22:51 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+  
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+  
+  private
+  
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+
+    if !consonant?(word[0])
+      word = word + "w"
+
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word + "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+end
